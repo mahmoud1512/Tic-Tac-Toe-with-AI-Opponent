584125a0bdce78c98c459cfc781181b6d0f23e01
mahmoud1512 <120992403+mahmoud1512@users.noreply.github.com>
2024-08-17 22:03:23 +0300

    Add GUI
    Add the basic GUI of the project

---
+++ Game/src/main/java/com/example/game/GameLauncher.java
@@ -0,0 +1,24 @@
+package com.example.game;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GameLauncher extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GameLauncher.class.getResource("Sign-in.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+        stage.setTitle("Tic-Tac-Toe");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ Game/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.game {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.game to javafx.fxml;
+    exports com.example.game;
+}
